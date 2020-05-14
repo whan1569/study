75b52e7e297ef06e1803486fc16061fd955b7ccb
whan1569 <48788071+whan1569@users.noreply.github.com>
2020-05-15 04:58:18 +0900

    Create MatchEvent.cs

---
+++ unity/기초/MatchEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchEvent : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Material mat;
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+    }
+
+    void OnCollisionEnter(Collision collision)          //충돌시
+    {
+        if(collision.gameObject.name == "MyBall")
+            mat.color = new Color(0, 0, 0);
+    }
+    void OnCollisionStay(Collision collision)           //충돌중
+    {
+
+    }
+    void OnCollisionExit(Collision collision)           //충동끝
+    {
+        if (collision.gameObject.name == "MyBall")
+            mat.color = new Color(1, 1, 1);
+    }
+}
